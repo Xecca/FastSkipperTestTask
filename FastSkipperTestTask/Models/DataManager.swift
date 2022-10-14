45cfce02b2dad13caea1793ff0a43190718be27a
Andrew Po <ensimgen@gmail.com>
2022-10-14 14:14:27 +0300

    Added description.

--- FastSkipperTestTask/Models/DataManager.swift
+++ FastSkipperTestTask/Models/DataManager.swift
@@ -15,41 +15,5 @@ protocol DataManagerDelegate {
 }
 
 struct DataManager {
-//    var motion = CMMotionManager()
-//    var timer = Timer()
-//    
-//    var delegate: DataManagerDelegate?
-//    
-//    mutating func startDeviceMotion() {
-//        if motion.isDeviceMotionAvailable {
-//            motion.deviceMotionUpdateInterval = 1.0 / 60.0
-//            motion.showsDeviceMovementDisplay = true
-//            motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
-//            
-//            // Configure a timer to fetch the motion data.
-//            timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true,
-//                          block: { [self] (timer) in
-//                if let data = motion.deviceMotion {
-//                    // Get the attitude relative to the magnetic north reference frame.
-//                    // pitch
-//                    let x = data.attitude.pitch
-//                    // heel/roll
-//                    let y = data.attitude.roll
-//                    let z = data.attitude.yaw
-//                    
-//                    var dataModel = DataModel()
-//                    dataModel.pitch = x
-//                    dataModel.heeling = y
-//                    // Use the motion data in your app.
-//                    
-//                    print("pitch: \(x)")
-//                    print("heel: \(y)")
-//                    self.delegate?.didUpdateData(self, motionData: dataModel)
-//                }
-//            })
-//            
-//            // Add the timer to the current run loop.
-//            RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
-//        }
-//    }
+
 }
